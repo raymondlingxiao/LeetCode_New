7caa540f48d932a0103ddceeba80f396233b1a09
raymondlingxiao <me@raymondlx.com>
2018-05-03 15:44:38 +0800

    1st Solution

---
+++ src/ReverseInteger_7.java
@@ -0,0 +1,23 @@
+public class ReverseInteger_7 {
+    class Solution1 {
+        public int reverse(int x) {
+            List<Integer> digits = new ArrayList<>();
+            int length = -1;
+            while(Math.abs(x)>=1){
+                digits.add(x%10);
+                x /= 10;
+                length ++;
+            }
+            long result = 0;
+            for (Integer digit: digits){
+                if (result < -Math.pow(2,31) || result> Math.pow(2,31)-1)
+                    return 0;
+                result += digit * Math.pow(10,length);
+                length --;
+            }
+            return (int)result;
+        }
+    }
+
+
+}
