ce14a5e7e22427ec77b4e8dd14dc96f7689dc108
Raymond <me@raymondllx.com>
2018-08-28 22:58:32 -0500

    8.28

--- src/GasStation_134.java
+++ src/GasStation_134.java
@@ -19,4 +19,33 @@ public class GasStation_134 {
             return -1;
         }
     }
+
+    class Solution2 {
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            int start = 0;
+            // if total gas > total cost, then must be a solution at start point
+            int total = 0;
+            int tank = 0;
+
+            for (int i=0;i<gas.length;i++){
+                tank += (gas[i] - cost[i]);
+                // if it can't reach the next point
+                if (tank < 0){
+                    // start from the next point and record the gas condition when pass it
+                    start = i+1;
+                    // the gas needed when reaching i+1
+                    total += tank;
+                    // new record from the next point
+                    tank = 0;
+                }
+            }
+
+            // when loop ends and tank still fullfill all the gas needed before and after this node
+            // return the start point
+            if (total + tank >= 0)
+                return start;
+            else
+                return -1;
+        }
+    }
 }
